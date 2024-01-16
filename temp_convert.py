fde5d6e873925c890fe83046ce87422ff7940c80
Aronk-112 <aronkvaszta12@outlook.com>
2024-01-16 16:37:56 +0100

    Forgot to commit

---
+++ temp_convert.py
@@ -0,0 +1,42 @@
+import argparse
+
+
+def convert(unit: str) -> float:
+    match unit:
+        case 'f':
+            return ()
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        description='Converts Fahrenheit to Celsius and vice versa.', 
+        usage= '''
+            py ./temp_convert.py 32 c (This means convert it to fahrenheit FROM celsius.)
+            py ./temp_convert.py 0  f (This means convert it to celsius FROM fahrenheit.)
+
+            If your command doesn't follow this structure, nothing happens.
+        ''')
+
+    parser.add_argument("Temparature", type=float, help='The value of temperature to convert.')
+    parser.add_argument("Unit", type=str, help='The unit of temperature, the value is in.')
+
+    arguments = parser.parse_args()
+
+    temperature_to_convert = arguments.Temparature
+    unit_to_convert_from: str = arguments.Unit
+
+    converted_temperature: float
+
+    match unit_to_convert_from.strip().lower():
+        case 'f':
+            converted_temperature = (temperature_to_convert - 32) * 5/9
+            print(f"{temperature_to_convert} Fahrenheit in Celsius is {converted_temperature}")
+        case 'c':
+            converted_temperature = (temperature_to_convert * 9/5) + 32
+            print(f"{temperature_to_convert} Celsius in Fahrenheit is {converted_temperature}")
+        case _:
+            print("I can't convert that.")
+
+
+if __name__ == '__main__':
+    main()
